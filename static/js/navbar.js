112feb242c99c579a2986600529f17199f437788
Zhao Huan <zhaohuanqdcn@gmail.com>
2025-09-18 14:58:45 +0800

    Fix navbar on mobile; update texts

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -2,9 +2,44 @@
 (function() {
   function initBurgerHandlers(root) {
     var $root = root ? $(root) : $(document);
-    $root.find('.navbar-burger').off('click').on('click', function() {
-      $('.navbar-burger').toggleClass('is-active');
-      $('.navbar-menu').toggleClass('is-active');
+    var $burger = $root.find('.navbar-burger');
+    var $menu = $root.find('.navbar-menu');
+    var $overlay = $root.find('.navbar-overlay');
+
+    function openSidebar() {
+      $burger.addClass('is-active');
+      $menu.addClass('is-active');
+      $overlay.addClass('is-active');
+      $('body').addClass('navbar-sidebar-open');
+    }
+
+    function closeSidebar() {
+      $burger.removeClass('is-active');
+      $menu.removeClass('is-active');
+      $overlay.removeClass('is-active');
+      $('body').removeClass('navbar-sidebar-open');
+    }
+
+    function toggleSidebar() {
+      if ($menu.hasClass('is-active')) closeSidebar(); else openSidebar();
+    }
+
+    $burger.off('click').on('click', function() {
+      toggleSidebar();
+    });
+
+    $overlay.off('click').on('click', function() {
+      closeSidebar();
+    });
+
+    // Close on navigation link click (use event delegation)
+    $root.find('.navbar-menu a').off('click.navclose').on('click.navclose', function() {
+      closeSidebar();
+    });
+
+    // Close on escape
+    $(document).off('keydown.navbar').on('keydown.navbar', function(e) {
+      if (e.key === 'Escape') closeSidebar();
     });
   }
 
@@ -13,7 +48,8 @@
     if ($container.length === 0) return;
     $.get('navbar.html', function(markup) {
       $container.html(markup);
-      initBurgerHandlers($container);
+      // Keep navbar where it's injected on docs page; no DOM relocation
+      initBurgerHandlers($(document));
     });
   }
 
